b00acff41e0aebcf610773986585d9a01e5b3e38
daedalussolutions <dylancassell000@hotmail.com>
2024-02-07 18:41:10 -0500

    Added description to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ApplicationRecord
     has_many :reviews
 
     validates :title, length: { maximum: 50 }, presence: true
+    validates :description, length: { maximum: 250 }, allow_blank: true
     validates :minimum_price, allow_blank: true, numericality: { greater_than: 0, less_than: 5000 }
     validates :maximum_price, allow_blank: true, numericality: { greater_than: 0, less_than: 5000 }
     validates :average_price, allow_blank: true, numericality: { greater_than: 0, less_than: 5000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_31_234444) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_211452) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.integer "price"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_234444) do
     t.float "average_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
