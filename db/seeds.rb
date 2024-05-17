df436c786ae2a7528487cc1f9187bf32128a354a
daedalussolutions <dylancassell000@hotmail.com>
2024-05-17 17:37:29 -0400

    Added admin account to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+User.create!([{
+    first_name: "admin",
+    last_name: "admin",
+    password: "password",
+    email: "admin@wrenchportal.com"
+}])
 Service.create!([{
     title: "Oil Change",
     minimum_price: 35.45,
