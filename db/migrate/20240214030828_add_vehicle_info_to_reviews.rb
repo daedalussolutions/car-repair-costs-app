cadcb245eab2f422c077e8ccfd2dd442998055c1
daedalussolutions <dylancassell000@hotmail.com>
2024-06-10 15:19:07 -0400

    Removed more unused migrations.

--- db/migrate/20240214030828_add_vehicle_info_to_reviews.rb
+++
@@ -1,7 +0,0 @@
-class AddVehicleInfoToReviews < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :reviews, :vehicle_make, foreign_key: true
-    add_reference :reviews, :vehicle_model, foreign_key: true
-    add_reference :reviews, :vehicle_year, foreign_key: true
-  end
-end
